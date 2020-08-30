8df5e84aa41b85e2489a66aab0c148ede63b2ab4
abd-alrahman01 <69123436+abd-alrahman01@users.noreply.github.com>
2020-08-30 03:44:57 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+var icon = document.getElementById("video-icon");
+
+
+
+
+
+
+function blankVideo(){
+    var video = document.getElementsByClassName("blank");
+    video.style.display = "none";
+}
