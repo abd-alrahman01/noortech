84c1f92fe818dfb53f658be4692095c12561b96f
abd-alrahman01 <69123436+abd-alrahman01@users.noreply.github.com>
2020-08-31 21:47:14 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,3 +2,13 @@ function toggle(){
     var popup = document.getElementById("container");
     popup.classList.toggle('active');
 }
+
+window.onscroll = function(){
+    var scroll = document.getElementById("arrow-up");
+    if(window.pageYOffset <= 100){
+        scroll.style.bottom = '-50px';
+    }
+    else{
+        scroll.style.bottom = '10px';
+    }
+}
